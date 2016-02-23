f84751a9c5fbe79f7162d2c1c4731cbe07fd2401
lsh9907 <lsh9907@hotmail.com>
2016-02-23 18:20:30 -0500

    111

--- app.js
+++ app.js
@@ -11,27 +11,30 @@ var mongoose = require('mongoose');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
-// add the article's route
+// add the articles route
 var articles = require('./routes/articles');
 
+// create an app
 var app = express();
 
 // db connection
 var db = mongoose.connection;
 
-// show an error if connection fails
+// show an error if connection fails (line 23 - 27 is just for testing)
 db.on('error', console.error.bind(console, 'DB Error: '));
-db.once('open', function(callback) {
+db.once('open', function(callback){
   console.log('Connected to mongodb');
 });
 
-//connect
+// connect
 mongoose.connect('mongodb://localhost/test');
 
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -41,17 +44,20 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 
-// map requests at /articles to use the routes/articles.js file
+//map requests at /articles to use he routes/articles.js file
 app.use('/articles', articles);
 
-
+// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
+// error handlers
 
+// development error handler
+// will print stacktrace
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
@@ -62,7 +68,8 @@ if (app.get('env') === 'development') {
   });
 }
 
-
+// production error handler
+// no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
