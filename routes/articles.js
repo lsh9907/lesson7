a742763b33e3a395570a23af51494fa14fead052
lsh9907 <lsh9907@hotmail.com>
2016-02-23 22:32:53 -0500

    1

--- routes/articles.js
+++ routes/articles.js
@@ -41,5 +41,50 @@ router.post('/add', function(req, res, next) {
     // redirect to main articles page
     res.redirect('/articles');
 });
+// GET handler for edit to show the populated form
+router.get('/:id', function(req, res, next) { // we make an id
+    // create an id variable to store the id from the url
+    var id = req.params.id;
+
+    // look up the selected article
+    Article.findById(id, function (err, article) {
+        if (err) {
+            console.log(err);
+            res.end(err);
+        } else {
+            // show the edit view
+            res.render('articles/edit', {
+                title: 'Article Details',
+                article: article
+            });
+        }
+    });
+    //res.render('articles/add', {
+    //    title: 'Add a New Article'
+    //});
+});
+// POST handler for edit to update the article
+router.post('/:id', function(req, res, next) {
+    // create an id variable to store the id from the url
+    var id = req.params.id;
+
+    // fill the article object
+    var article = new Article({
+        _id: id,
+        title: req.body.title,
+        content: req.body.content
+    });
+
+    // use mongoose and our Article model to update
+    Article.update({_id: id}, article, function(err) {
+            if (err) {
+                console.log(err)
+                res.end(err);
+            } else {
+                res.redirect('/articles');
+            }
+        });
+});
+
 // make public
 module.exports = router;
